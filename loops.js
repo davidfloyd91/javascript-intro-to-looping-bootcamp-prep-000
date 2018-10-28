7aafdb1b724b805829682d704a197abdfbee29d6
David Floyd <davidfloyd91@gmail.com>
2018-10-28 02:10:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,3 +11,10 @@ function forLoop(array) {
 
 //Create a function called whileLoop in loops.js. The function should take a number as an argument.
 //Using a while loop, count down (using console.log) from the passed in number to 0. Then return the string 'done'.
+
+function whileLoop(number) {
+  while (number >= 0) {
+    console.log(--number);
+  }
+}
+return 'done';
